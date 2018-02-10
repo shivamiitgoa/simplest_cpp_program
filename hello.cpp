98957cf8dffe87e87e4bcdd0739dbd920043d5ba
Shivam Kumar <31388987+shivamiitgoa@users.noreply.github.com>
2018-02-10 21:58:14 +0530

    Added Code of Simplest Program. Version - 1

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// This is the library that we are using
+#include <iostream>
+using namespace std;
+
+// this is the start of main function
+// main function runs as soon as the program starts
+int main() {
+  // cout is used to print inside the console
+  cout << "Hello" << endl;
+}
